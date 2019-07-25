d46ce97d5c94865e851c6d2417e1df74eb6d07bc
Hoa Thiên Vũ <hoathienvu8x@gmail.com>
2019-07-25 10:18:07 +0700

    Update brule.php code

--- brule.php
+++ brule.php
@@ -2,7 +2,7 @@
 $all = file('rules.txt');
 $rules = array();
 foreach($all as $i => $r) {
-    if (preg_match('/embed|author|comment|json|trackback/i',$r)) {
+    if (preg_match('/embed|comment|json|trackback/i',$r)) {
         continue;
     }
     if (preg_match('/feed\//i',$r)) {
@@ -18,11 +18,16 @@ foreach($all as $i => $r) {
         $ctl = 'DisplayPostHandle';
     } else if (preg_match('/\?tag=/',$r)) {
         $ctl = 'DisplayTagHandle';
-    } else if (preg_match('/\?category_name=/',$r)) { 
+    } else if (preg_match('/\?category_name=/',$r)) {
         $ctl = 'DisplayCategoryHandle';
     } else if (preg_match('/sitemap/',$r)) {
         $ctl = 'DisplaySiteMapHandle';
+    } else if (preg_match('/search\//',$r)) {
+        $ctl = 'DisplaySearchHandle';
+    } else if (preg_match('/author\//',$r)) {
+        $ctl = 'DisplayAuthorHandle';
     }
+
     $r = str_replace('(feed|rdf|rss|rss2|atom)','(feed|rss|atom)',$r);
     $r = str_replace('category_name','name',$r);
     $r = str_replace('?tag=','?name=',$r);
@@ -44,7 +49,8 @@ foreach($all as $i => $r) {
 //foreach($all as $a) {
 foreach($rules as $a) {
     $a[0] = str_replace('\\.', '\\\\.', $a[0]);
-    echo "        // ".trim($a[2])."\n        Route{\n            Pattern:\"".$a[0]."\",\n            Params:[]string{\"".implode('","', $a[1])."\"},\n            Controller: ".$a[3].",\n        },\n";
+    // echo "        // ".trim($a[2])."\n        Route{\n            Pattern:\"".$a[0]."\",\n            Params:[]string{\"".implode('","', $a[1])."\"},\n            Controller: ".$a[3].",\n        },\n";
+    echo "        Route{\n            Pattern:\"".$a[0]."\",\n            Params:[]string{\"".implode('","', $a[1])."\"},\n            Controller: ".$a[3].",\n        },\n";
 }
 
 //print_r($all);
